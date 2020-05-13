38845865f7a30f59697052069fd6e6e020f4b29b
mikemaczynski <maczynski.mike@gmail.com>
2020-05-12 21:51:30 -0500

    Added buttons to refresh weather manually, as well as change the weather API key through a popup menu

--- weatherbar.py
+++ weatherbar.py
@@ -4,22 +4,31 @@ class WeatherBar(object):
 
     def __init__(self):
         self.app = rumps.App("WeatherBar")
-        self.set_up_menu()
-
-        self.setWeatherButton = rumps.MenuItem(title="Update Weather", callback=self.setWeather)
-        self.app.menu = [self.setWeatherButton]
-
-    def set_up_menu(self):
         self.app.title = "Weather"
 
-    def setWeather(self, sender):
-            sender.title = "Weather Updated"
-            self.app.title = "Temperature"
-
-    def run(self):
+        self.setAPIKeyButton = rumps.MenuItem(title="Set API Key", callback=self.setAPIKey)
+        self.app.menu.add(self.setAPIKeyButton)
+        self.setWeatherButton = rumps.MenuItem(title="Refresh Weather", callback=self.setWeather)
+        self.app.menu.add(self.setWeatherButton)
+        self.app.menu.add(rumps.separator)
         self.app.run()
 
+    def setWeather(self, sender):
+        sender.title = "Weather Updated"
+        self.app.title = "Temperature"
 
+    def setAPIKey(self, sender):
+        updateAPI = rumps.Window(
+            message='Enter your weather API key',
+            title='Weather API Key',
+            ok="Update API Key",
+            dimensions=(300, 23)
+        )
+        updateAPI.icon = "location.png"
+        updateButton = updateAPI.run()
+
+        if updateButton.clicked:
+            print(updateButton.text)
+        
 if __name__ == '__main__':
     app = WeatherBar()
-    app.run()
